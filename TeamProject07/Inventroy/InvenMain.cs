c52d371cf7ecc89a3e10b0dab7c79307d884e9d7
a-ou-d <148048020+a-ou-d@users.noreply.github.com>
2023-11-19 19:43:49 +0900

    테스트 문제로 statview/login 삭제 - invenmain 수정

--- TeamProject07/Inventroy/InvenMain.cs
+++ TeamProject07/Inventroy/InvenMain.cs
@@ -17,79 +17,91 @@ namespace TeamProject07.Inventroy
         {
             Define.MainGamePhase choicePhase;
             Console.Clear();
-            Console.WriteLine("보유 중인 아이템을 확인하고 관리할 수 있습니다.");
-            Console.WriteLine("\n");
-            Console.WriteLine("[아이템 목록]");
-            Console.WriteLine("\n");
-            Console.WriteLine("[장비 아이템]");
 
-            foreach (Item item in player.Inven) // 장비 아이템 리스트
+            Console.SetCursorPosition(1, 1);
+            Console.WriteLine("보유 중인 아이템을 확인하고 관리할 수 있습니다."); // (1, 1)
+
+            Console.SetCursorPosition(1, 2);
+            Console.WriteLine("상세 정보를 보고 싶은 아이템을 선택해주세요."); // (1, 2)
+
+            for (int i = 5; i < 30; i++)  // (3, 50++)로 가운데 분리선 그림
             {
-                int i = 0;
-                i++;
-
-                string equippedSign = item.IsEquipped ? "[E]" : "";
-                string input_str = $"[{i}] {item.Name} {equippedSign}";
-                string output = InsertLineBreakEveryTwentyCharacters(input_str); // 20글자마다 줄바꿈 호출
-                Console.WriteLine(output);
+                Console.SetCursorPosition(50, i);
+                Console.Write("|");
             }
 
-            Console.WriteLine("[소모 아이템]");
+            Console.SetCursorPosition(15, 4);
+            Console.WriteLine("[아이템 목록]"); // [아이템 목록] - (15, 4)
+            Console.SetCursorPosition(75, 4);
+            Console.WriteLine("[아이템 정보]"); // [아이템 정보] - (75, 4)
 
-            foreach (Item item in player.Inven) // 소모 아이템 리스트
+            int selectedItemIndex = ItemList(player);
+            ItemDescription(player, selectedItemIndex);
+
+
+            static void ItemList(Player player)
             {
-                int i = 0;
-                i++;
+                int startListY = 0;
 
-                string input_str = $"[{i}] {item.Name}";
-                string output = InsertLineBreakEveryTwentyCharacters(input_str); // 20글자마다 줄바꿈
-                Console.WriteLine(output);
-            }
-
-            static string InsertLineBreakEveryTwentyCharacters(string text) // 20글자마다 줄바꿈 메소드
-            {
-                string result = "";
-                for (int i = 0; i < text.Length; i++)
+                foreach (Item item in player.Inven)
                 {
-                    if (i > 0 && i % 20 == 0)
-                        result += "\n";
-                    result += text[i];
+
+                    string equippedSign = item.IsEquipped ? "[E]" : "";
+                    string input_str = $"[{startListY + 1}] {equippedSign} {item.Name}";
+
+                    for (int j = 0; j < input_str.Length; j += 10)
+                    {
+                        string substring = input_str.Substring(j, Math.Min(10, input_str.Length - j));
+                        Console.SetCursorPosition(12, 6 + startListY + j / 10); // (12, 6)에서 출력 시작해 아이템 번호가 바뀌거나, 아이템 이름이 10글자를 넘어가면 y값 바꿈
+                        Console.Write($"{substring}");
+                    }
+
+                    startListY += (input_str.Length + 9) / 10;
                 }
-                return result;
+
+                int selectedItemIndex = CheckValidInput(0, startListY);
+                return selectedItemIndex;
             }
 
-
-            int input = CheckValidInput(0, 0);
-            switch (input)
+            static void ItemDescription(Player player, int selectedItemIndex)
             {
-                case 0:
-                    choicePhase = Define.MainGamePhase.Main;
-                    break;
+                if (selectedItemIndex >= 0 && selectedItemIndex < player.Inven.Count)
+                {
+                    Item selectedItem = player.Inven[selectedItemIndex];
 
+                    int startDescriptionY = 6;
 
+                    //""부분 수정해야함
+                    Console.SetCursorPosition(75, startDescriptionY++); // 출력위치
+                    WriteLineInParts("타입", 75, ref startDescriptionY); //타입
+                    WriteLineInParts("이름", 75, ref startDescriptionY); //이름
+                    WriteLineInParts("설명", 75, ref startDescriptionY); //설명
+                    WriteLineInParts("올라가는 능력치", 75, ref startDescriptionY); //올라가는 능력치
+                    WriteLineInParts("세트 이름", 75, ref startDescriptionY); //세트 이름
+                    WriteLineInParts("가격", 75, ref startDescriptionY); //가격
+                }
+                else
+                {
+                    Console.SetCursorPosition(75, 6);
+                    Console.WriteLine("잘못된 선택입니다");
+                }
+
+                Console.SetCursorPosition(75, startDescriptionY);
+                Console.WriteLine("0. 돌아가기");
             }
-            return Define.MainGamePhase.Main;
         }
 
-        /*Console.WriteLine("[설명]");
-
-        for (int i = 0; i<player.inven.Count; i++)
+        static void WriteLineInParts(string text, int startX, ref int startDescriptionY)
         {
-            //string input = "종류 : ";
-            //string input = "세트명 : ";
-            //string input = "장비부위 : ";
-            //string input = "능력치 : ";
-            //string input = "설명 : ";
-            //string input = "착용직업 : ";
-            //string input = "가격 : ";
+            for (int i = 0; i < text.Length; i += 10)
+            {
+                string part = text.Substring(i, Math.Min(10, text.Length - i));
+                Console.SetCursorPosition(startX, startDescriptionY++);
+                Console.WriteLine(part);
+            }
+        }
 
-            // 변수이름 전부 수정해야함
-
-            string output = InsertLineBreakEveryTwentyCharacters(input);
-        Console.WriteLine(output);
-        }*/
-
-    private int CheckValidInput(int min, int max)
+        private int CheckValidInput(int min, int max)
         {
             while (true)
             {
