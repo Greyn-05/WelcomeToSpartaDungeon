ddaf97766e355094cecbc5ab14b2bb050e3b094c
levell1 <whddnr0317@naver.com>
2023-11-20 19:47:00 +0900

    [Fix,ADD] Player,Monster 턴,  클래스는 참조타입-> 문제 -> 객체생성

--- TeamProject07/Dungeon/Dungeon.cs
+++ TeamProject07/Dungeon/Dungeon.cs
@@ -22,7 +22,6 @@ namespace TeamProject07.Controller
     
     internal class DungeonEntrance
     {
-        public Player player = new Player("1", 1, 1, 1, 1, 1, 1, 1);
         
         public void InDungeon(Monster monster)
         {
@@ -99,13 +98,10 @@ namespace TeamProject07.Controller
         public void StartDungeon(int stage)
         {
 
-            int monsternum = 1;
-            if (stage == 2) { monsternum = 4; }            //난이도
-            else if (stage == 3) { monsternum = 7; }
+            int monsternum = 101;
+            if (stage == 2) { monsternum = 201; }            //난이도
+            else if (stage == 3) { monsternum = 301; }
 
-            /*int monsternum = 100;
-            if (stage == 2) { monsternum = 200; }            //난이도
-            else if (stage == 3) { monsternum = 300; }*/
 
             CreateMonsters = new List<Monster>();
             CreateMonsters.Clear();
@@ -116,11 +112,13 @@ namespace TeamProject07.Controller
             for (int i = 0; i < MonsterNumber; i++)
             {
 
-                MonsterType = rand.Next(monsternum, monsternum + 3);   //몬스터 데이터 보고 조정   
-                Monster monsterinfo = monsterData.ElementAt(MonsterType).Value;
-                //Monster monsterinfo = monsterData[101];
-                CreateMonsters.Add(monsterinfo);
-                Console.WriteLine($"LV.{monsterinfo.Level} \t {monsterinfo.Name} \t HP : {monsterinfo.Hp} \t ATK : {monsterinfo.Attack},");
+                MonsterType = rand.Next(monsternum, monsternum + 2);   //몬스터 데이터 보고 조정   
+                Monster monsterinfo = monsterData[MonsterType];
+
+                Monster m = new Monster(monsterinfo);
+
+                CreateMonsters.Add(m);
+                //Console.WriteLine($"LV.{monsterinfo.Level} \t {monsterinfo.Name} \t HP : {monsterinfo.Hp} \t ATK : {monsterinfo.Attack},");
             }
         }
 
@@ -148,11 +146,12 @@ namespace TeamProject07.Controller
         public void PlayerPhase(Player player)
         {
             Console.Clear();
-            Console.WriteLine("\n전투가 시작됩니다!!\n\n");
+            Console.WriteLine("\n\t전투가 시작됩니다!!");
+            
             int killMonsterNum = 0;
-            while (!player.IsDead || MonsterNumber != killMonsterNum)//&& !CreateMonsters[0].IsDead
+            while (!player.IsDead)
             {
-
+                Console.WriteLine($"\n\t   {player.Name} 체력 :{player.Hp} \n");
                 for (int i = 0; i < CreateMonsters.Count; i++)
                 {
                      
@@ -168,6 +167,7 @@ namespace TeamProject07.Controller
                 Console.WriteLine("\n공격할 몬스터를 선택하세요.");
                 int monsterChoice = CheckValidInput(0, MonsterNumber-1);
                 Console.Clear();
+                Console.WriteLine($"\n\t   {player.Name} 체력 :{player.Hp} \n");
                 //선택된 몬스터의 글자 색이 바뀌는 코드가 추가되면 좋겠어요
                 for (int i = 0; i < MonsterNumber; i++)
                 {
@@ -190,38 +190,109 @@ namespace TeamProject07.Controller
                     }
 
                 }
-                Console.WriteLine("공격할 스킬을 선택하세요.");
+                Console.WriteLine("\n공격할 스킬을 선택하세요.\n");
+                Console.WriteLine($"번호 \t이름\t데미지");
+                for (int i = 1; i <= player.Skills.Count; i++) { 
+                
+                Console.WriteLine($"{i}. \t{player.Skills[i].Name} \t{player.Skills[i].Damage}");
+
+                }
                 int skillChoice = CheckValidInput(1, player.Skills.Count);
                 Console.Clear();
+                Console.WriteLine($"\n\t   {player.Name} 체력 :{player.Hp} \n");
                 if (CreateMonsters[monsterChoice].IsDead == false)
                 {
-                    CreateMonsters[monsterChoice].TakeDamage(player, player.Skills[skillChoice].Damage);
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write($"\n  {player.Name} "); Console.ResetColor(); Console.Write("가 공격합니다. ");
+                    int damageValue =CreateMonsters[monsterChoice].TakeDamage(player, player.Skills[skillChoice].Damage);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write($"\n{CreateMonsters[monsterChoice].Name} "); Console.ResetColor(); Console.Write("님이 공격하였습니다.");
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write($" {player.Name} "); Console.ResetColor(); Console.Write("가");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write($" {damageValue} "); Console.ResetColor(); Console.Write($"의 피해를 받았습니다!\n\n");
+                    Thread.Sleep(3000);
+                    Console.Clear();
                     if (CreateMonsters[monsterChoice].IsDead==true)
                     {
                         killMonsterNum++;
                     }
+                    if (MonsterNumber == killMonsterNum)
+                    {
+                        WinBoard(CreateMonsters, player);
+                        
+                        break;
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("올바른 몬스터를 선택하십시오");
+                    Console.WriteLine("잘못 입력하셨습니다. 턴이 소모됩니다.");
                 }
+                monsterPhase(player);
             }
+            
 
         }
 
+        public void monsterPhase(Player player)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write($"\n 몬스터 "); Console.ResetColor(); Console.Write("가 공격합니다. ");
+            for (int i = 0; i < CreateMonsters.Count; i++)
+            {
+                if (!player.IsDead)
+                {
+                    if (!CreateMonsters[i].IsDead)
+                    {
+                        int damageValue = player.TakeDamage(CreateMonsters[i], 0);
+                        player.Hp -= damageValue;
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write($"\n{player.Name} "); Console.ResetColor(); Console.Write("이/가");
+
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write($" {CreateMonsters[i].Name} "); Console.ResetColor(); Console.Write("에게");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write($" {damageValue} "); Console.ResetColor(); Console.WriteLine($"의 피해를 받았습니다!\n\n");
+
+                        Console.WriteLine($"\t   {player.Name} 체력 :{player.Hp} \n");
+                        Thread.Sleep(3000);
+                        
+                    }
+                }
+                else {
+                    Console.Write($" {player.Name} 가 사망하였습니다. ");
+                    player.Hp = 1;
+                    Thread.Sleep(4000);
+                    break;
+                }
+            }
+            Console.Clear();
+
+        }
+
+       
         public void WinBoard(List<Monster> monster, Player player)
         {
-            int rewardGold;
-            int rewardExp;
-
-            rewardGold = monster[0].Gold + monster[1].Gold + monster[2].Gold;
-            rewardExp = monster[0].RewardExp + monster[1].RewardExp + monster[2].RewardExp;
+            int rewardGold= 0;
+            int rewardExp= 0;
+            for (int i = 0; i < monster.Count; i++)
+            {
+                rewardGold = rewardGold + monster[i].Gold ;
+                rewardExp = rewardGold + monster[i].RewardExp;
+            }
             player.Gold += rewardGold;
             player.LevelUpExp += rewardExp;
-            Console.WriteLine("승리하셨습니다!");
-            Console.WriteLine($"획득한 골드 : {0}", rewardGold);
-            Console.WriteLine($"획득한 경험치 : {0}", rewardExp);
-            Console.WriteLine("0.돌아가기");
+            Console.WriteLine($"\n\t\t{player.Name}님 승리하셨습니다!\n");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"\t획득한 골드 : {rewardGold}");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"\t획득한 EXP  : {rewardExp}\n\n");
+            Console.ResetColor();
+            Console.WriteLine($"{player.Name} 정보");
+            Console.WriteLine($"\tGold  : {player.Gold}");
+            Console.WriteLine($"\tLevel : {player.Level} , EXP : {player.LevelUpExp}");
+            
+            Console.WriteLine("\n\n\t\t0.돌아가기");
             switch (CheckValidInput(0, 0))
             {
                 case 0
