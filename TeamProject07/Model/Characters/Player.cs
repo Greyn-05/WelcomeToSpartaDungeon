d4cf04b596568d3c10e07ffdbeade915b5cbea10
JacksonPepperoni <mashpota1014@gmail.com>
2023-11-22 16:27:08 +0900

    플레이어 코드 백업

--- TeamProject07/Model/Characters/Player.cs
+++ TeamProject07/Model/Characters/Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeamProject07.Inventroy;
 using TeamProject07.Items;
 using TeamProject07.Skills;
 using TeamProject07.Utils;
@@ -18,12 +19,10 @@ namespace TeamProject07.Characters
 
         public int MaxMp;
 
-        public Define.SetEquip set;
-
         public Dictionary<int, Skill> Skills;
 
         public Equipment EquipStats { get; set; }
-
+        public ConsumableItem ConsumStats { get; set; }
         public Player(string name, string _class, int level, int attack, int defence, int hp, int mp, int gold, int critRate, int missRate)
         {
             Name = name;
@@ -49,6 +48,23 @@ namespace TeamProject07.Characters
         public int TotalMp => MaxMp + EquipStats.AddMp;
         public float TotalCritRate => CritRate + EquipStats.AddCritRate;
         public float TotalMissRate => MissRate + EquipStats.AddMissRate;
+        /*
+         Weapon,
+         Head,
+         Body,
+         Hand,
+         Foot,
+         Accessory
+
+
+      */
+
+        public Define.SetEquip set;
+
+
+        public int[] equipParts = new int[6];
+
+
 
         public void TotalStats()
         {
@@ -58,6 +74,8 @@ namespace TeamProject07.Characters
             EquipStats.AddMp = 0;
             EquipStats.AddCritRate = 0;
             EquipStats.AddMissRate = 0;
+            ConsumStats.BuffHp = 0;
+            ConsumStats.BuffMp = 0;
 
             foreach (Item item in Inven)
             {
@@ -87,61 +105,116 @@ namespace TeamProject07.Characters
                             break;
                     }
                 }
-            }
-        }
-
-        public void EquipItem(Item item)
-        {
-            item.IsEquipped = !item.IsEquipped;
-            TotalStats();
-        }
-
-        public void LoadSkills(string SKpath)
-        {
-            Skills = new Dictionary<int, Skill>();
-            Skills.Clear();
-
-            if (File.Exists(SKpath))
-            {
-                using (StreamReader sr = new StreamReader(new FileStream(SKpath, FileMode.Open)))
+                else if (item.IsUsed)
                 {
-                    sr.ReadLine();
-
-                    while (!sr.EndOfStream)
+                    switch (item.buff)
                     {
-                        string line = sr.ReadLine();
-                        string[] data = line.Split(',');
+                        case Define.Buff.hp:
+                            ConsumStats.BuffHp += item.buffValue;
+                            break;
+                        case Define.Buff.mp:
+                            ConsumStats.BuffMp += item.buffValue;
+                            break;
+                        default:
+                            break;
+                    }
 
-                        //Skill skill = new Skill(data[0], data[1], data[2], data[3]);
-                        Skill skill = new Skill();
-                        skill.Id = int.Parse(data[0]);
-                        skill.Name = data[1];
-                        skill.Damage = int.Parse(data[2]);
-                        skill.Mp = int.Parse(data[3]);
+                }
 
-                        Skills.Add(skill.Id, skill);
+
+
+
+                if (InvenMain.SetItemCheck()) // 세트효고ㅡㅏ있으면 세트 추가
+                {
+                    switch (set)
+                    {
+
+                        case Define.SetEquip.신문지세트:
+                            EquipStats.AddCritRate += 3;
+                            EquipStats.AddMissRate += 5;
+                            break;
+                        case Define.SetEquip.천세트:
+                            EquipStats.AddAttack += 4;
+                            EquipStats.AddDefence += 6;
+                            EquipStats.AddMissRate += 10;
+                            break;
+                        case Define.SetEquip.나무세트:
+                            EquipStats.AddAttack += 10;
+                            EquipStats.AddDefence += 2;
+                            EquipStats.AddMissRate += 6;
+                            EquipStats.AddHp += 20;
+                            EquipStats.AddMp += 20;
+                            break;
+                        case Define.SetEquip.강철세트:
+                            EquipStats.AddDefence += 30;
+                            EquipStats.AddCritRate += 5;
+                            EquipStats.AddMissRate += 3;
+                            EquipStats.AddHp += 30;
+                            EquipStats.AddMp += 30;
+                            break;
+
+                        case Define.SetEquip.세트능력없음:
+                        default:
+                            break;
                     }
                 }
             }
+
         }
 
-        public Define.MainGamePhase ShowSkillProto()
-        {
-            Define.MainGamePhase choicePhase;
-            Console.Clear();
-            Console.WriteLine("Load된 스킬정보");
-            foreach (KeyValuePair<int, Skill> s in Skills)
+
+        public void EquipItem(Item item)
             {
-                Console.WriteLine($"key : {s.Key}, ID : {s.Value.Id}, Name : {s.Value.Name}, Damage : {s.Value.Damage}, MP : {s.Value.Mp}");
+                item.IsEquipped = !item.IsEquipped;
+                TotalStats();
             }
-            Console.WriteLine("아무키나 입력");
-            Console.ReadLine();
-            return Define.MainGamePhase.Main;
+
+            public void LoadSkills(string SKpath)
+            {
+                Skills = new Dictionary<int, Skill>();
+                Skills.Clear();
+
+                if (File.Exists(SKpath))
+                {
+                    using (StreamReader sr = new StreamReader(new FileStream(SKpath, FileMode.Open)))
+                    {
+                        sr.ReadLine();
+
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            string[] data = line.Split(',');
+
+                            //Skill skill = new Skill(data[0], data[1], data[2], data[3]);
+                            Skill skill = new Skill();
+                            skill.Id = int.Parse(data[0]);
+                            skill.Name = data[1];
+                            skill.Damage = int.Parse(data[2]);
+                            skill.Mp = int.Parse(data[3]);
+
+                            Skills.Add(skill.Id, skill);
+                        }
+                    }
+                }
+            }
+
+            public Define.MainGamePhase ShowSkillProto()
+            {
+                Define.MainGamePhase choicePhase;
+                Console.Clear();
+                Console.WriteLine("Load된 스킬정보");
+                foreach (KeyValuePair<int, Skill> s in Skills)
+                {
+                    Console.WriteLine($"key : {s.Key}, ID : {s.Value.Id}, Name : {s.Value.Name}, Damage : {s.Value.Damage}, MP : {s.Value.Mp}");
+                }
+                Console.WriteLine("아무키나 입력");
+                Console.ReadLine();
+                return Define.MainGamePhase.Main;
+            }
+
+            //public void LevelUp()
+            //{
+
+            //}
         }
-
-        //public void LevelUp()
-        //{
-
-        //}
     }
-}
